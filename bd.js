a25317b7d1c69b45c4c90e6f3cb06af2b9523b32
Renzo Remuzgo <renzo.remuzgo@tecsup.edu.pe>
2024-04-11 10:47:22 -0500

    Stack2

--- bd.js
+++ bd.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1/Stack_meanBD';
+const url = 'mongodb://127.0.0.1/data';
 
 mongoose.connect(url, {
     useNewUrlParser: true,
